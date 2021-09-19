6f44b1d8b031e8d1ce39f243f4769d09c559d42e
Vladislav <vg_evsyukov@betboom.org>
2021-09-20 01:56:56 +0300

    update

--- sheets.py
+++ sheets.py
@@ -9,10 +9,7 @@ creds_shop = {'user': os.environ.get('BASIC_LOGIN_SHOP'), 'pass': os.environ.get
 creds_provider = {'user': os.environ.get('BASIC_LOGIN_COUNT'), 'pass': os.environ.get('BASIC_PASS_COUNT')}
 
 transactions_info = get_transctions(url_provider_metrics, creds=creds_provider)
-print(transactions_info)
-# print(get_transctions(url_shop_metrics, creds=creds_shop))
-
-#authorization
+# transactions_all = get_transctions(url_shop_metrics, creds=creds_shop)
 gc = pygsheets.authorize(service_file='creds.json')
 
 # Create empty dataframe
@@ -21,19 +18,19 @@ df = pd.DataFrame()
 # Create a column
 ## ALL TRANSACTIONS
 
-df['date_from'] = [transactions_info['report_params']['from']]
-df['date_to'] = [transactions_info['report_params']['to']]
-df['successful_count'] = [transactions_info['all']['successful_count']]
-df['failed_count'] = [transactions_info['all']['failed_count']]
-df['ratio'] = [transactions_info['ratio']]
+df['date_from'] = [transactions_info['report_params']['from'], transactions_info['report_params']['from'], transactions_info['report_params']['from']]
+df['date_to'] = [transactions_info['report_params']['to'], transactions_info['report_params']['to'], transactions_info['report_params']['to']]
+df['payment_method'] = ['credit_card', 'alternative', 'erip']
+df['successful_count'] = [transactions_info['credit_card']['successful'], transactions_info['alternative']['successful'], transactions_info['erip']['successful']]
+df['failed_count'] = [transactions_info['credit_card']['failed'], transactions_info['alternative']['failed'], transactions_info['erip']['failed']]
+df['pending_count'] = [transactions_info['credit_card']['pending'], transactions_info['alternative']['pending'], transactions_info['erip']['pending']]
+df['ratio'] = [transactions_info['credit_card']['ratio'], transactions_info['alternative']['ratio'], transactions_info['erip']['ratio']]
 
 #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
 sh = gc.open('Transactions')
-
-print(dir(sh[0]))
 #select the first sheet
 wks = sh[0]
 
 #update the first sheet with df, starting at cell B2.
 
-wks.set_dataframe(df,(2,1))
+wks.set_dataframe(df,(1,1))
--- transactions.py
+++ transactions.py
@@ -7,7 +7,9 @@ def http_request_post(url, payload, headers=None, creds=None):
 
     if creds and headers:
         headers.update({'Content-Type': 'application/json'})
+        print(url, creds.get('user'), creds.get('pass'))
         response = requests.post(url, headers=headers, json=payload, auth=(creds.get('user'), creds.get('pass')))
+        print(response.text)
         return response.json()
 
     if headers:
@@ -20,17 +22,18 @@ def http_request_post(url, payload, headers=None, creds=None):
         return response.json()
 
 
-
 def get_request_body(status, payment_method):
 
     time_zone = datetime.timedelta(minutes=180)
     gap = datetime.timedelta(minutes=30)
 
+
+
     request_body = {
         "report_params": {
             "date_type": "paid_at",
             "from": str((datetime.datetime.now() - time_zone - gap).strftime('%Y-%m-%d %H:%M')),
-            "to": str((datetime.datetime.now() - datetime.timedelta(minutes=180)).strftime('%Y-%m-%d %H:%M')),
+            "to": str((datetime.datetime.now() - time_zone).strftime('%Y-%m-%d %H:%M')),
             "status": str(status),
             "payment_method_type": str(payment_method),
             "time_zone": "UTC"}
@@ -38,106 +41,91 @@ def get_request_body(status, payment_method):
     return request_body
 
 
-def get_transctions(url, creds, notify=None):
+def get_transctions(url, creds):
+
+    def get_ratio(success, failed):
+        if failed + success == 0:
+            ratio = 0
+            return ratio
+
+        ratio = float("{0:.2f}".format((success / (failed + success) * 100)))
+        return ratio
 
     successful_count = 0
     failed_count = 0
+    pending_count = 0
     payment_methods_info = {"credit_card": {
-                                'successful_count': successful_count,
-                                'failed_count': failed_count},
-                      "alternative": {
-                                'successful_count': successful_count,
-                                'failed_count': failed_count},
-                        "erip": {
-                                'successful_count': successful_count,
-                                'failed_count': failed_count},
-                        "all": {
-                            'successful_count': successful_count,
-                            'failed_count': failed_count}
-                    }
+                                'successful': successful_count,
+                                'failed': failed_count,
+                                'pending': pending_count},
+
+                            "alternative": {
+                                'successful': successful_count,
+                                'failed': failed_count,
+                                'pending': pending_count},
+
+                            "erip": {
+                                'successful': successful_count,
+                                'failed': failed_count,
+                                'pending': pending_count},
+
+                            "all": {
+                                'successful': successful_count,
+                                'failed': failed_count,
+                                'pending': pending_count}
+                            }
 
     headers = {'X-Api-Version': '3'}
+    for method in ['credit_card', 'alternative', 'erip']:
+        for status in ["successful", "failed", 'pending']:
+            body = get_request_body(status, method)
+            result = http_request_post(url, body, headers, creds)
+            payment_methods_info['credit_card'][status] += result.get('transactions').get('count')
+            payment_methods_info['all'][status] += result.get('transactions').get('count')
 
-    body = get_request_body("successful", 'credit_card')
-    result = http_request_post(url, body, headers, creds)
-    payment_methods_info['credit_card']['successful_count'] += result.get('transactions').get('count')
-    payment_methods_info['all']['successful_count'] += result.get('transactions').get('count')
-    body.update(payment_methods_info)
-    # print(body)
-
-    ####
-
-    body = get_request_body("failed", 'credit_card')
-    result = http_request_post(url, body, headers, creds)
-    payment_methods_info['credit_card']['failed_count'] += result.get('transactions').get('count')
-    payment_methods_info['all']['failed_count'] += result.get('transactions').get('count')
     body.update(payment_methods_info)
 
-    body.update({'ratio': float("{0:.2f}".format((payment_methods_info['all']['successful_count'] / (payment_methods_info['credit_card']['failed_count'] + payment_methods_info['all']['successful_count']) * 100)))})
-    if notify:
-
-        message = {
-            'date_from': (datetime.datetime.now() - datetime.timedelta(minutes=60)).strftime('%Y-%m-%d %H:%M'),
-            'date_to': datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),
-            'successful_count': payment_methods_info['all']['successful_count'],
-            'failed_count': payment_methods_info['credit_card']['failed_count'],
-            'ratio': float("{0:.2f}".format((payment_methods_info['all']['successful_count'] / (payment_methods_info['credit_card']['failed_count'] + payment_methods_info['all']['successful_count']) * 100)))
-        }
-        return message
-
+    for method in ['credit_card', 'alternative', 'erip']:
+        ratio = get_ratio(payment_methods_info[method]['successful'], payment_methods_info[method]['failed'])
+        body[method]['ratio'] = ratio
     return body
 
 
-def get_reports(url, creds):
-    successful_count = 10
-    failed_count = 10
-    payment_method = ("credit_card", "alternative")
-    # transaction_status = ("successful", "failed")
+def get_transaction_report(url, creds):
+    transaction_info = {
+        'Time': '',
+        'Description': '',
+        'Amount': 0,
+        'Currency': '',
+        'PaymentGatewayName': '',
+        'PaymentMethodType': '',
+        'AdditionalData': {
+            'Fraud': {
+                'Rules': {
+                    'show': ''}}},
+        'Status': ''}
 
     headers = {'X-Api-Version': '3'}
-
-
-    for method in payment_method:
-        result = http_request_post(url, get_request_body("successful", method), headers, creds)
-        successful_count += result.get('transactions').get('count')
-
-        result = http_request_post(url, get_request_body("failed", method), headers, creds)
-        failed_count += result.get('transactions').get('count')
-
-    message = {
-        'date_from': (datetime.datetime.now() - datetime.timedelta(minutes=60)).strftime('%Y-%m-%d %H:%M'),
-        'date_to': datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),
-        'successful_count': successful_count,
-        'failed_count': failed_count,
-        'ratio': float("{0:.2f}".format((successful_count / (failed_count + successful_count) * 100)))
-    }
-    return message
+    for method in ['credit_card', 'alternative', 'erip']:
+        for status in ["successful", "failed"]:
+            body = get_request_body(status, method)
+            result = http_request_post(url, body, headers, creds)
+            transaction_info['Time'] += result.get('transactions').get('count')
+            transaction_info['all'][status] += result.get('transactions').get('count')
+    pass
+    return body
 
 
 def send_notify(contacts, message):
-    text_message = f"Constantpos API. Total stats from {message.get('date_from')}  to {message.get('date_to')}: " \
-              f"Accept amount: {message.get('successful_count')}\n" \
-              f"Decline amount: {message.get('failed_count')}\n" \
+    generate_message = f"Constantpos API. Total stats from {message.get('report_params').get('date_from')}  to {message.get('report_params').get('date_to')}: " \
+              f"Accept amount: {message.get('all').get('successful_count')}\n" \
+              f"Decline amount: {message.get('all').get('failed')}\n" \
               f"Accept ratio: {message.get('ratio')} %"
 
-
     url = f'https://api.telegram.org/bot{os.environ.get("TELEGRAM_BOT_TOKEN")}/sendMessage'
-
     for contact in contacts:
         telegram_json_body = {
           "chat_id": contact,
-          "text": text_message
+          "text": generate_message
         }
-        print(telegram_json_body)
         http_request_post(url, telegram_json_body)
-
-
-# url_shop_metrics = os.environ.get('URL_REPORTS')
-# url_provider_metrics = os.environ.get('URL_COUNT')
-# creds_shop = {'user': os.environ.get('BASIC_LOGIN_SHOP'), 'pass': os.environ.get('BASIC_PASS_SHOP')}
-# creds_provider = {'user': os.environ.get('BASIC_LOGIN_COUNT'), 'pass': os.environ.get('BASIC_PASS_COUNT')}
-# message = get_transctions(url_shop_metrics, creds_shop)
-# message2 = get_transctions(url_provider_metrics, creds_provider)
-# print(message2)
-# contacts = ['-457443920']
-#send_notify(contacts, message)
